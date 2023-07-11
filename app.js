1a9009092db101d1eb5ef674d392e8ffab8a0917
Neeraj Kumar <56425717+neerajdaksh@users.noreply.github.com>
2023-07-11 17:22:08 +0530

    fix random image changes

--- app.js
+++ app.js
@@ -1,35 +1,39 @@
+//second image
 //generate the random number
 var randomNumber1 = Math.floor(Math.random()*6)+1;
+//generate random image
 var randomDiceeImage1 = "dice"+randomNumber1+".png";
+//generate random image source
 var randomImageSource1 = "./images/"+randomDiceeImage1;
-
+//select first image
+var firstImage = document.querySelectorAll('img')[0];
+//set attribute in image
+firstImage.setAttribute('src',randomImageSource1);
+//second image
 var randomNumber2 = Math.floor(Math.random()*6)+1;
 var randomDiceeImage2 = "dice"+randomNumber2+".png";
 var randomImageSource2 = "./images/"+randomDiceeImage2;
-
-var firstImage = document.querySelectorAll('img')[0];
 var secondImage = document.querySelectorAll('img')[1];
-
-
-firstImage.setAttribute('src',randomImageSource1);
 secondImage.setAttribute('src',randomImageSource2);
- 
 
-var heading = document.querySelector('h1');
 
 
  
-//processing
+
+ 
+
+ 
+//processing if player 1 is wins
 if(randomNumber1>randomNumber2){
     //output
-    heading.innerHTML = "🚩 Player 1 Wins!";
+    document.querySelector('h1').innerHTML = "🚩 Player 1 Wins!";
 }
 else if(randomNumber1<randomNumber2){
     //output
-    heading.innerHTML = "Player 2 Wins! 🚩";
+    document.querySelector('h1').innerHTML = "Player 2 Wins! 🚩";
 }
 else{
     //output
-    heading.innerHTML = "Draw!😁";
+    document.querySelector('h1').innerHTML = "Draw!😁";
     
 }
